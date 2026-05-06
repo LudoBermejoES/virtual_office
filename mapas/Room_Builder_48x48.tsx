7d85dea086ac7048806e3508de574fca6c5cc8f7
LudoBermejoES <97520290+LudoBermejoES@users.noreply.github.com>
2026-05-06 17:28:13 +0200

    New map

---
+++ mapas/Room_Builder_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Room_Builder_48x48" tilewidth="48" tileheight="48" tilecount="8588" columns="76">
+ <image source="Room_Builder_48x48.png" width="3648" height="5424"/>
+</tileset>
